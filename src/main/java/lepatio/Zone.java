b5bb6374e7286c2e44d5484ae1f64375b5e0f316
jocooper <jocooper2018@gmail.com>
2024-06-05 13:48:48 +0200

    Ajout de la classe Zone

---
+++ src/main/java/lepatio/Zone.java
@@ -0,0 +1,28 @@
+package lepatio;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class Zone {
+    
+    private String nom;
+    private Set<Spectacle> spectacles;
+    
+    public Zone(String nom, Spectacle premierSpectacle) {
+        this.nom = nom;
+        this.spectacles = new HashSet<Spectacle>();
+        this.spectacles.add(premierSpectacle);
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public Set<Spectacle> getSpectacles() {
+        return spectacles;
+    }
+}
