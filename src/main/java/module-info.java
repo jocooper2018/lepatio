199b977aa4eb7781d0b0bbd142d02336a657c902
bzhkylian <bzhkylian@localhost.localdomain>
2024-06-07 10:03:47 +0200

    ajout main

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,6 +2,9 @@ module lepatio {
     requires javafx.controls;
     requires javafx.fxml;
 
-    opens lepatio to javafx.fxml;
+    opens lepatio to javafx.fxml, java.graphics, java.base;
+    opens main to javafx.fxml, java.graphics, java.base;
+    opens modele to javafx.fxml, java.graphics, java.base;
     exports lepatio;
+
 }
