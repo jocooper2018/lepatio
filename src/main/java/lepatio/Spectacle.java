a5199d6486cbffdc1a829febc92f8a9dff8622fc
jocooper <jocooper2018@gmail.com>
2024-06-10 12:29:52 +0200

    Début de la doc de la classe Spectacle

--- src/main/java/lepatio/Spectacle.java
+++ src/main/java/lepatio/Spectacle.java
@@ -3,8 +3,15 @@ package lepatio;
 import java.util.HashSet;
 import java.util.Set;
 
+/**
+ * La classe {@code Spectacle} représente un spectacle.
+ * Un spectacle peut faire l'objet de plusieurs représentations.
+ * 
+ * @author LE BOUT Matthieu
+ * @see Representation
+ */
 public class Spectacle {
-    
+
     private String nom;
     private int duree;
     private int nbreMaxSpect;
@@ -13,8 +20,24 @@ public class Spectacle {
     private Set<Representation> representations;
     private Set<Zone> zones;
     private static Set<Tarif> tarifs = new HashSet<Tarif>();
-    
-    public Spectacle(String nom, int duree, int nbreMaxSpect, Genre genre, Artiste premierArtiste, Zone premiereZone, long pleinTarifPremiereZone) {
+
+    /**
+     * Construit un nouveau spectacle avec les paramètres spécifiés.
+     * 
+     * @param nom                    Le nom du spectacle.
+     * @param duree                  La durée du spectacle.
+     * @param nbreMaxSpect           Le nombre maximum de spectateurs pouvant
+     *                               assister à une représentation du spectacle.
+     * @param genre                  Le genre du spectacle.
+     * @param premierArtiste         Le premier artiste à ajouter à la liste
+     *                               d'artiste.
+     * @param premiereZone           La première zone à ajouter à la liste des
+     *                               zones.
+     * @param pleinTarifPremiereZone Le plein tarif associé à la première zone à
+     *                               ajouter.
+     */
+    public Spectacle(String nom, int duree, int nbreMaxSpect, Genre genre, Artiste premierArtiste, Zone premiereZone,
+            long pleinTarifPremiereZone) {
         this.setNom(nom);
         this.setDuree(duree);
         this.setNbreMaxSpect(nbreMaxSpect);
@@ -26,18 +49,34 @@ public class Spectacle {
         this.ajouterZone(premiereZone, pleinTarifPremiereZone);
     }
 
+    /**
+     * Retourne le nom du spectacle.
+     * @return Le nom du spectacle.
+     */
     public String getNom() {
         return nom;
     }
 
+    /**
+     * Modifie le nom du spectacle.
+     * @param nom Le nouveau nom du spectacle.
+     */
     public void setNom(String nom) {
         this.nom = nom;
     }
 
+    /**
+     * Retourne la durée du spectacle.
+     * @return La durée du spectacle.
+     */
     public int getDuree() {
         return duree;
     }
 
+    /**
+     * Modifie la durée du spectacle.
+     * @param duree La nouvelle durée du spectacle.
+     */
     public void setDuree(int duree) {
         this.duree = duree;
     }
