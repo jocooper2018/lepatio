4cc5ba7d2b67d98c493913ccf19b2054ee97a0fe
jocooper <jocooper2018@gmail.com>
2024-06-10 15:33:18 +0200

    Documentation Artiste

--- src/main/java/lepatio/Artiste.java
+++ src/main/java/lepatio/Artiste.java
@@ -3,34 +3,78 @@ package lepatio;
 import java.util.HashSet;
 import java.util.Set;
 
+/**
+ * La classe {@code Artiste} représente un artiste.
+ * Un artiste à un nom et interprète plusieurs spectacles.
+ * Un artiste doit interpréter au moins un spectacle.
+ * 
+ * @author LE BOUT Matthieu
+ * @see Spectacle
+ */
 public class Artiste {
 
     private String nom;
     private Set<Spectacle> spectacles;
 
+    /**
+     * Construit un artiste avec son nom et son premier spectacle.
+     * 
+     * @param nom              Nom de l'artiste.
+     * @param premierSpectacle Premier spectacle interprété par l'artiste.
+     */
     public Artiste(String nom, Spectacle premierSpectacle) {
         this.nom = nom;
         this.spectacles = new HashSet<Spectacle>();
         this.spectacles.add(premierSpectacle);
     }
 
+    /**
+     * 
+     * @return Le nom de l'artiste.
+     */
     public String getNom() {
         return nom;
     }
 
+    /**
+     * Change le nom de l'artiste.
+     */
     public void setNom(String nom) {
         this.nom = nom;
     }
 
+    /**
+     * 
+     * @return L'ensemble des spectacles interprété par l'artiste.
+     */
     protected Set<Spectacle> getSpectacles() {
         return spectacles;
     }
 
+    /**
+     * Ajoute un spectacle à l'ensemble des spectacles interprété par l'artiste.
+     * 
+     * @param spectacle Le spectacle à ajouter.
+     * @return {@code true} si l'ensemble des spectacles interprété par l'artiste ne
+     *         contient pas déjà le spectacle à ajouter.
+     */
     protected boolean ajouterSpectacle(Spectacle spectacle) {
         return this.getSpectacles().add(spectacle);
     }
 
-    protected boolean enleverSpectacle(Spectacle spectacle) {
+    /**
+     * Enlève un spectacle de l'ensemble des spectacles interprété par l'artiste.
+     * 
+     * @param spectacle Le spectacle à enlever.
+     * @return {@code true} si l'ensemble des spectacles interprété par l'artiste
+     *         contient le spectacle à enlever.
+     * @throws IllegalStateException
+     */
+    protected boolean enleverSpectacle(Spectacle spectacle) throws IllegalStateException {
+        if (this.getSpectacles().size() <= 1) {
+            throw new IllegalStateException(
+                    "Impossible de supprimer le spectacle : l'artiste doit interpréter au moins un spectacle.");
+        }
         return this.getSpectacles().remove(spectacle);
     }
 
@@ -63,5 +107,4 @@ public class Artiste {
     public String toString() {
         return "Artiste [nom=" + nom + "]";
     }
-
 }
