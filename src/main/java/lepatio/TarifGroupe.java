ade6185a4ac9495aca72e67631bc47ca0e6a1319
jocooper <jocooper2018@gmail.com>
2024-06-11 18:35:10 +0200

    Documentation TarifGroupe

--- src/main/java/lepatio/TarifGroupe.java
+++ src/main/java/lepatio/TarifGroupe.java
@@ -1,9 +1,25 @@
 package lepatio;
 
+/**
+ * La classe {@code TarifGroupe} représente une tarification qui s'applique aux
+ * d'au moins 20 adultes. Cette tarification apporte une réduction de 15%.
+ * 
+ * @author LE BOUT Matthieu
+ * @see Billet
+ * @see Tarification
+ * @see TarifJeune
+ * @see TarifAdulte
+ * @see TarifSenior
+ * @see TarifAbonne
+ */
 public class TarifGroupe extends Tarification {
     
     public static final double reductionStatic = 0.15;
     
+    /**
+     * Construit un TarifGroupe
+     */
+
     public TarifGroupe() {
         super();
     }
