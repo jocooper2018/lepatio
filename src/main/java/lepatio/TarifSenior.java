45ea93a134c1a964522e8c9aaf77c24d3a66d37e
jocooper <jocooper2018@gmail.com>
2024-06-11 18:30:25 +0200

    Documentation TarifSenior

--- src/main/java/lepatio/TarifSenior.java
+++ src/main/java/lepatio/TarifSenior.java
@@ -1,5 +1,17 @@
 package lepatio;
 
+/**
+ * La classe {@code TarifSenior} représente une tarification qui s'plique aux
+ * senior de plus de 65 ans. Cette tarification apporte une réduction de 25%.
+ * 
+ * @author LE BOUT Matthieu
+ * @see Billet
+ * @see Tarification
+ * @see TarifAdulte
+ * @see TarifSenior
+ * @see TarifGroupe
+ * @see TarifAbonne
+ */
 public class TarifSenior extends Tarification {
     
     public static final double reductionStatic = 0.25;
