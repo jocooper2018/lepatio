a7ffa75ac988251cec0ef404fd606ff94875a213
jocooper <jocooper2018@gmail.com>
2024-06-11 18:23:51 +0200

    Documentation TarifJeune

--- src/main/java/lepatio/TarifJeune.java
+++ src/main/java/lepatio/TarifJeune.java
@@ -1,9 +1,24 @@
 package lepatio;
 
+/**
+ * La classe {@code TarifJeune} représente une tarification au tarif jeune
+ * associé à un billet. Cette tarification apporte une réductions de 30%.
+ * 
+ * @author LE BOUT Matthieu
+ * @see Billet
+ * @see Tarification
+ * @see TarifAdulte
+ * @see TarifSenior
+ * @see TarifGroupe
+ * @see TarifAbonne
+ */
 public class TarifJeune extends Tarification {
-    
+
     public static final double reductionStatic = 0.3;
 
+    /**
+     * Construit un TarifJeune
+     */
     public TarifJeune() {
         super();
     }
