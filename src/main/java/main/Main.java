33e21d523bf27848e68b7e2ebda17073325dc861
jocooper <jocooper2018@gmail.com>
2024-06-12 09:54:38 +0200

    Ajout de la méthode créer Genre

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -4,38 +4,51 @@ import javafx.application.Application;
 import javafx.scene.control.ListView;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
+import lepatio.Donnees;
 import lepatio.Genre;
 import modele.*;
 
 public class Main extends Application {
-	private static FenListeSpec fListe;
-	private static FenCreerGenre fCreer;
-	private static ListView<Genre> ctrlListe;
+    private static FenListeSpec fListe;
+    private static FenCreerGenre fCreer;
+    private static ListView<Genre> ctrlListe;
 
-	@Override
-	public void start(Stage primaryStage) throws Exception {
+    @Override
+    public void start(Stage primaryStage) throws Exception {
 
-		Main.fListe = new FenListeSpec();
-		Main.fCreer = new FenCreerGenre();
+        Main.fListe = new FenListeSpec();
+        Main.fCreer = new FenCreerGenre();
 
-		Main.fCreer.initModality(Modality.APPLICATION_MODAL);
-		this.fListe.show();
-	}
+        Main.fCreer.initModality(Modality.APPLICATION_MODAL);
+        this.fListe.show();
+    }
 
-	static public void fermerAppli() {
-		System.exit(0);
-	}
+    // Gestion des fenêtres
 
-	static public void fermerPopup() {
-		Main.fCreer.close();
-	}
+    public static void fermerAppli() {
+        System.exit(0);
+    }
 
-	static public void ouvrirCreerGenre() {
-		Main.fCreer.show();
-	}
+    public static void fermerPopup() {
+        Main.fCreer.close();
+    }
 
-	public static void main(String[] args) {
-		Application.launch(args);
-	}
+    public static void ouvrirCreerGenre() {
+        Main.fCreer.show();
+    }
+
+    // Gestion des données
+
+    public static void creerGenre(Genre genre) {
+        Donnees.ajouterGenre(genre);
+    }
+
+    public static void creerGenre(String nomGenre) {
+        Main.creerGenre(new Genre(nomGenre));
+    }
+
+    public static void main(String[] args) {
+        Application.launch(args);
+    }
 
 }
